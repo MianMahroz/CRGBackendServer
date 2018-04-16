a6cfbe49763ee43f1b3fbc6d8167d71e5ab4e76a
MianMahroz <mianmahroz1@gmail.com>
2018-04-15 22:34:06 -0700

    first commit

---
+++ src/main/java/com/bestercapitalmedia/chiragh/CRGBackendServer/CrgBackendServerApplication.java
@@ -0,0 +1,12 @@
+package com.bestercapitalmedia.chiragh.CRGBackendServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrgBackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrgBackendServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bestercapitalmedia/chiragh/CRGBackendServer/CrgBackendServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bestercapitalmedia.chiragh.CRGBackendServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrgBackendServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
